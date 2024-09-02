7c660cb9c7f9b19c893f93c54e3f0979bcbd838f
Rani Panchore <138985031+ranipanchore@users.noreply.github.com>
2024-09-02 18:00:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 function connect () {
     mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
+    // mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
+    mongoose.connect('mongodb+srv://panchorerani1:uu4cjdj3BBdMNqUo@tododb.rvfuohx.mongodb.net/et/',{useNewUrlParser: true})
 }
 
 module.exports = connect
