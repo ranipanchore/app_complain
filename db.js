c5e1fe8d2d89fd86d4e6eb54ac774ead212e4629
rani panchore <panchorerani1@gmail.com>
2024-09-03 10:29:39 +0530

    changes done

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 function connect () {
     mongoose.set('useCreateIndex', true);
-    // mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
-    mongoose.connect('mongodb+srv://panchorerani1:uu4cjdj3BBdMNqUo@tododb.rvfuohx.mongodb.net/et/',{useNewUrlParser: true})
+    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
+   
 }
 
 module.exports = connect
